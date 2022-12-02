55382dd3455fccd1634b2212d5df6477c43f03fc
fyq <1210915690@qq.com>
2022-12-02 15:34:45 +0800

    add backend

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,19 +1,42 @@
 <template>
+  <div>昵称：{{bot_name}}</div>
+  <div>级别：{{bot_rating}}</div>
   <NavBar />
   <router-view/>
 </template>
 
-<script>
+<script >
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap/dist/js/bootstrap'
 import NavBar from './components/NavBar.vue'
+import $ from 'jquery'
+import { ref } from 'vue'
 
 export default {
   name: "App",
   components: {
     NavBar,
+  },
+  setup: () => {
+    let bot_name = ref("");
+    let bot_rating = ref("");
+
+    $.ajax({
+      url: "http://localhost:3000/pk/getbotinfo/",
+      type: "get",
+      success: resp => {
+        console.log(resp);
+      }
+    });
+
+    return {
+      bot_name,
+      bot_rating
+    }
   }
+
 }
+
 </script>
 
 <style>
---
+++ kob/src/main/java/com/kob/backend/KobApplication.java
@@ -0,0 +1,13 @@
+package com.kob.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KobApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KobApplication.class, args);
+    }
+
+}

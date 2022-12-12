d810b0b2a55f81d87833071a763734db5c374fed
fyq <1210915690@qq.com>
2022-12-13 00:37:03 +0800

    add usertable

--- frontend/src/views/ranklist/RanklistIndexView.vue
+++ frontend/src/views/ranklist/RanklistIndexView.vue
@@ -1,6 +1,35 @@
 <template>
     <ContentBase>
-        用户
+        <table class="table table-striped table-hover" style="text-align: center;">
+            <thead>
+                <tr>
+                    <th>用户</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="user in users" :key="user.id">
+                    <td>
+                        <img :src="user.photo" alt="" class="record-user-photo">
+                        &nbsp;
+                        <span class="record-user-username">{{ user.username }}</span>
+                    </td>
+                    <td>{{ user.rating }}</td>
+                </tr>
+            </tbody>
+        </table>
+        <nav aria-label="...">
+        <ul class="pagination" style="float: right;">
+            <li class="page-item" @click="click_page(-2)">
+                <a class="page-link" href="#">前一页</a>
+            </li>
+            <li :class="'page-item ' + page.is_active" v-for="page in pages" :key="page.number" @click="click_page(page.number)">
+                <a class="page-link" href="#">{{ page.number }}</a>
+            </li>
+            <li class="page-item" @click="click_page(-1)">
+                <a class="page-link" href="#">后一页</a>
+            </li>
+        </ul>
+        </nav>
     </ContentBase>
 </template>
 
@@ -8,7 +37,7 @@
 import ContentBase from '../../components/ContentBase.vue';
 import $ from 'jquery'
 import { useStore } from 'vuex'
-
+import { ref } from 'vue';
 
 export default {
     components: {
@@ -16,55 +45,65 @@ export default {
     },
     setup() {
         const store = useStore();
-        // $.ajax({
-        //     url: "http://127.0.0.1:3000/user/bot/add/",
-        //     type: "POST",
-        //     data: {
-        //         content: "Bot的代码22",
-        //     },
-        //     headers: {
-        //         Authorization: "Bearer " + store.state.user.token,
-        //     },
-        //     success(resp) {
-        //         console.log(resp);
-        //     },
-        //     error(resp) {
-        //         console.log(resp);
-        //     }
-        // })
+        let users = ref([]);
+        let current_page = 1;
+        let total_users = 0;
+        let pages = ref([]);
 
-        // $.ajax({
-        //     url: "http://127.0.0.1:3000/user/bot/remove/",
-        //     type: "POST",
-        //     data: {
-        //         id: 1,
-        //     },
-        //     headers: {
-        //         Authorization: "Bearer " + store.state.user.token,
-        //     },
-        //     success(resp) {
-        //         console.log(resp);
-        //     },
-        //     error(resp) {
-        //         console.log(resp);
-        //     }
-        // })
+        const click_page = page => {
+            if (page === -2) page = current_page - 1;
+            else if (page === -1) page = current_page + 1;
+            let max_pages = parseInt(Math.ceil(total_users / 10));
 
-        $.ajax({
-            url: "http://127.0.0.1:3000/user/bot/update/",
-                type: "POST",
+            if (page >= 1 && page <= max_pages) {
+                pull_page(page);
+            }
+        }
+
+        const udpate_pages = () => {
+            let max_pages = parseInt(Math.ceil(total_users / 10));
+            let new_pages = [];
+            for (let i = current_page - 2; i <= current_page + 2; i ++ ) {
+                if (i >= 1 && i <= max_pages) {
+                    new_pages.push({
+                        number: i,
+                        is_active: i === current_page ? "active" : "",
+                    });
+                }
+            }
+            pages.value = new_pages;
+        }
+
+        const pull_page = page => {
+            current_page = page;
+            $.ajax({
+                url: "http://127.0.0.1:3000/ranklist/getlist/",
                 data: {
-                    id: 2,
-                    title: "666",
-                    content: "你好牛奥",
+                    page,
                 },
+                type: "get",
                 headers: {
                     Authorization: "Bearer " + store.state.user.token,
                 },
                 success(resp) {
+                    users.value = resp.users;
+                    console.log(users.value);
+                    total_users = resp.users_count;
+                    udpate_pages();
+                },
+                error(resp) {
                     console.log(resp);
                 }
             })
+        }
+
+        pull_page(current_page);
+
+        return {
+            users,
+            pages,
+            click_page
+        }
     }
 }
 </script>
